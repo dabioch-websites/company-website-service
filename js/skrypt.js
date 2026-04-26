1de8ebba8fcb844ee8e71ab9e9454838960e9044
Dabioch Web <psowatypsowaty@gmail.com>
2026-04-26 20:57:21 +0200

    Add files via upload

---
+++ js/skrypt.js
@@ -0,0 +1,7 @@
+// MENU MOBILE
+const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
